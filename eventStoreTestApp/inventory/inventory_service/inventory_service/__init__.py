b18691e98ce8ef3e67c06e23ee3274866aff43f7
Kertész Dávid <kerteszdavid11@gmail.com>
2021-02-03 12:13:11 +0100

    wire WSGI into Flask for Prometheus

--- eventStoreTestApp/inventory/inventory_service/inventory_service/__init__.py
+++ eventStoreTestApp/inventory/inventory_service/inventory_service/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
 
 app = Flask(__name__)
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 from inventory_service import views
