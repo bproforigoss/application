1b1b2d5ddaf8d52fe0c92b8e2fdb3e44449869ab
rubrumvulpes <36009166+rubrumvulpes@users.noreply.github.com>
2021-02-11 10:31:38 +0100

    fix black errors

--- eventStoreTestApp/inventory/inventory_service/inventory_service/prom_logs.py
+++ eventStoreTestApp/inventory/inventory_service/inventory_service/prom_logs.py
@@ -1,16 +1,14 @@
 from prometheus_client import Counter, Summary
 
 performance_metrics = {
-    "http_counter": Counter(
-        "http_requests", "HTTP requests served"
-        ),
+    "http_counter": Counter("http_requests", "HTTP requests served"),
     "events_produced": Counter(
-            "events_produced", "Events sent out by this microservice"
-        ),
+        "events_produced", "Events sent out by this microservice"
+    ),
     "http_request_duration": Summary(
         "http_request_duration", "Duration of HTTP request being served"
-        ),
+    ),
     "event_send_duration": Summary(
         "event_send_duration", "Duration of sending an event"
-        )
+    )
 }
