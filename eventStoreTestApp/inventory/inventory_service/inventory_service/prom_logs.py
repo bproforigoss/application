edb3be32cb8636505a21bc6fb5de51c2a4d216b8
rubrumvulpes <36009166+rubrumvulpes@users.noreply.github.com>
2021-02-17 14:41:02 +0100

    black linter fix

--- eventStoreTestApp/inventory/inventory_service/inventory_service/prom_logs.py
+++ eventStoreTestApp/inventory/inventory_service/inventory_service/prom_logs.py
@@ -4,11 +4,9 @@ performance_metrics = {
     "http_request_summary": Summary(
         "http_request_summary", "Summary of HTTP request being served"
     ),
-    "event_send_summary": Summary(
-        "event_send_summary", "Summary of sending events"
-    ),
+    "event_send_summary": Summary("event_send_summary", "Summary of sending events"),
     "connection_error_counter": Counter(
         "connection_error_counter",
-        "Counter of errors related to connection to database services"
+        "Counter of errors related to connection to database services",
     ),
 }
