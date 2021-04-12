d49beccd72f9fd37c40a0ef135f9dc57177bbcbe
Kertész Dávid <kerteszdavid11@gmail.com>
2021-04-12 14:21:02 +0200

    log format in specific key-value pairs

--- eventStoreTestApp/order_interface/order_service/order_service/__init__.py
+++ eventStoreTestApp/order_interface/order_service/order_service/__init__.py
@@ -7,7 +7,7 @@ from flask import Flask
 
 logging.basicConfig(
     stream=sys.stdout,
-    format="%(name)s - %(levelname)s - %(message)s",
+    format="app=order_service where=%(filename)s level=%(levelname)s msg='%(message)s'",
     level=logging.INFO,
 )
 
