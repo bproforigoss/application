fe6cb35ecca0e317e05b503d4c9ddf67ca0bb050
Kertész Dávid <kerteszdavid11@gmail.com>
2021-04-12 15:48:45 +0200

    replace single quotes with double in log format

--- eventStoreTestApp/order_interface/order_service/order_service/__init__.py
+++ eventStoreTestApp/order_interface/order_service/order_service/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 logging.basicConfig(
     stream=sys.stdout,
-    format="app=order_service where=%(filename)s level=%(levelname)s msg='%(message)s'",
+    format='app=order_service where=%(filename)s level=%(levelname)s msg="%(message)s"',
     level=logging.INFO,
 )
 
